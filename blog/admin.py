98f7feb9a1dc44736968eb50fcea4fb6dfedba28
matt080313 <heonwon0704@gmail.com>
2019-08-10 16:40:01 +0900

    changed post_list.html

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from blog.models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image', 'description')
+    list_display = ('title', 'image', 'description','create_date','modify_date')
 
 
 admin.site.register(Post, PostAdmin)
